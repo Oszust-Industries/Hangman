7d0481596469aba54d1087eb3e1743f813abfad9
Soszust40 <soszust40@gmail.com>
2025-08-23 18:21:01 -0400

    Fixed Mac Building

--- build_mac.sh
+++ build_mac.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 python3 -m PyInstaller Bin/Main.py \
     --noconsole \
+    --noconfirm \
+    --hidden-import=PyQt6 \
+    --hidden-import=PyQt6.QtCore \
+    --hidden-import=PyQt6.QtGui \
+    --hidden-import=PyQt6.QtWidgets \
     --add-data "Data:Data" \
     --add-data "Bin:Bin" \
     --add-data "Achievement Icons:Achievement Icons" \
